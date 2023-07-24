4822e0eb33656581fa357a942087da37daf0e79c
dev-rakesh-88 <dev.rakeshkrishnan@gmail.com>
2023-07-24 12:10:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import AudioRecorder from "../src/AudioRecorder";
+
+const App = () => {
+   
+    
+    return (
+        <div>
+            <h1>React Media Recorder</h1>
+            <div className="button-flex">
+                
+            </div>
+            <div>
+                <AudioRecorder />
+            </div>
+        </div>
+    );
+};
+export default App;
